e5bf2d218a44042a0f6ff1f865d9a638f52cec92
KotaMorikawa <koke.kokota0130@gmail.com>
2025-04-20 17:38:39 +0900

    E2Eテスト設定

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
+    headless: false, // ヘッドモードを有効に設定
   },
   projects: [
     {
